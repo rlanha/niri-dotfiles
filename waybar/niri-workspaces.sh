f16e0457194d775c8dada6591d007b445d65e4e8
rlanha <haryoairlangga@gmail.com>
2026-08-16 10:09:40 +0700

    Show workspace order indices in waybar

--- waybar/niri-workspaces.sh
+++ waybar/niri-workspaces.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-niri msg workspaces | tail -n +4 | while IFS='|' read -r id name output windows is_active is_focused urgent; do
-    id="$(echo "$id" | xargs)"
-    is_focused="$(echo "$is_focused" | xargs)"
-    if [[ "$is_focused" == '*' ]]; then
-        printf '<span foreground="#ffb52a">%s</span> ' "$id"
+niri msg --json workspaces 2>/dev/null | jq -r '
+    sort_by(.idx)[] |
+    if .is_focused then
+        "<span foreground=\"#ffb52a\">" + (.idx|tostring) + "</span> "
     else
-        printf '<span foreground="#93A1A1">%s</span> ' "$id"
-    fi
-done
+        "<span foreground=\"#93A1A1\">" + (.idx|tostring) + "</span> "
+    end'
