dc26f9a869e29cb1ed1dfcde81d6fc3e95d09ac7
rlanha <haryoairlangga@gmail.com>
2026-08-16 10:21:40 +0700

    Show only focused workspace number

--- waybar/niri-workspaces.sh
+++ waybar/niri-workspaces.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 niri msg --json workspaces 2>/dev/null | jq -r '
-    sort_by(.idx)[] |
-    if .is_focused then
-        "<span foreground=\"#ffb52a\">" + (.idx|tostring) + "</span>"
-    else
-        "<span foreground=\"#93A1A1\">" + (.idx|tostring) + "</span>"
-    end' | tr '\n' ' '
+    .[] | select(.is_focused) | .idx'
