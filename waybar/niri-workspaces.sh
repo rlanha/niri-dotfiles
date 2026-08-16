0ec8972544a413f51e9d4f3e8798e080b371aa0e
rlanha <haryoairlangga@gmail.com>
2026-08-16 10:17:56 +0700

    Fix waybar workspaces rendering on a single line

--- waybar/niri-workspaces.sh
+++ waybar/niri-workspaces.sh
@@ -2,7 +2,7 @@
 niri msg --json workspaces 2>/dev/null | jq -r '
     sort_by(.idx)[] |
     if .is_focused then
-        "<span foreground=\"#ffb52a\">" + (.idx|tostring) + "</span> "
+        "<span foreground=\"#ffb52a\">" + (.idx|tostring) + "</span>"
     else
-        "<span foreground=\"#93A1A1\">" + (.idx|tostring) + "</span> "
-    end'
+        "<span foreground=\"#93A1A1\">" + (.idx|tostring) + "</span>"
+    end' | tr '\n' ' '
